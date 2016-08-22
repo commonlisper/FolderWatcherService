56ddecf73b0400ab2fb4ea1453dd6a895651c54e
Dmytro Pavlenko <atosdo@live.com>
2016-08-22 18:31:56 +0300

    Add the bat files for register/unregister service

---
+++ FolderWatcherService/bat/register.bat
@@ -0,0 +1,13 @@
+﻿@echo OFF
+
+set DOTNET = %SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\
+set PATH = %PATH%;%DOTNET%
+
+echo register FolderWatcher Service...
+echo ------------------------------------------------------
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil /i "%~dp0FolderWatcherService.exe"
+net start FolderWatcherService
+services.msc
+echo ------------------------------------------------------
+
+echo Done!	
---
+++ FolderWatcherService/bat/unregister.bat
@@ -0,0 +1,12 @@
+﻿@echo OFF
+
+set DOTNET = %SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\
+set PATH = %PATH%;%DOTNET%
+
+echo unregister FolderWatcher Service...
+echo ------------------------------------------------------
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil /u "%~dp0FolderWatcherService.exe"
+services.msc
+echo ------------------------------------------------------
+
+echo Done!
